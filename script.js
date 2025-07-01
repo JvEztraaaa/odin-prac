c820a6a06e51139c257f2ac6000b808656a24a40
JvEztraaaa <180340662+JvEztraaaa@users.noreply.github.com.>
2025-07-02 03:01:35 +0800

    to upper

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+function toUpper(string) {
+  return string.toUpperCase();
+}
+
 const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
 
-for (const cat of cats) {
-  console.log(cat);
-}
+// Apply `toUpper()` to each item in the array using .map()
+const upperCats = cats.map(toUpper);
+
+console.log(upperCats);
+// Output: [ "LEOPARD", "SERVAL", "JAGUAR", "TIGER", "CARACAL", "LION" ]

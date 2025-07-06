0c50eaef610111c513558f7482a07048e00480df
JvEztraaaa <180340662+JvEztraaaa@users.noreply.github.com>
2025-07-07 02:01:40 +0800

    rps practice

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-const cats = ["Pete", "Biggles", "Jasmine"];
-
-let myFavoriteCats = "My cats are called ";
-
-for (const cat of cats) {
-  myFavoriteCats += `${cat}, `;
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomChoice = Math.floor(Math.random() * 3);
+  return choices[randomChoice];
 }
 
-console.log(myFavoriteCats); // "My cats are called Pete, Biggles, Jasmine, "
+function getHumanChoice() {
+  const humanChoice = prompt(`Choose between: rock/paper/scissors`);
+  return humanChoice;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+console.log(`Human choice: ${humanSelection}`);
+
+const computerSelection = getComputerChoice();
+console.log(`Computer choice: ${computerSelection}`);
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+  } else if (humanChoice === computerChoice) {
+    console.log(`It's a tie!`);
+  } else {
+    console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
+  }
+}
+
+playRound(humanSelection, computerSelection);

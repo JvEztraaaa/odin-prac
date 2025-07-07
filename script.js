404cdfc7ce66c297a94668cc334314ccad0838c6
JvEztraaaa <180340662+JvEztraaaa@users.noreply.github.com>
2025-07-08 03:01:32 +0800

    matrix multid

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let arr = ["Apple", "Orange", "Pear"];
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
 
-for (let i = 0; i < arr.length; i++) {
-  alert( arr[i] );
-}
+alert( matrix[0][1] )

9e06b33a86acf230970159d8de79c9adc804ab74
JvEztraaaa <180340662+JvEztraaaa@users.noreply.github.com>
2025-07-08 02:36:29 +0800

    prime

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-let num;
+let n = prompt("no:");
 
-do {
-  num = prompt("Enter a number greater than 100?", 0);
-} while (num <= 100);
+nextPrime:
+for (let i = 2; i <= n; i++) { // for each i...
+
+  for (let j = 2; j < i; j++) { // look for a divisor..
+    if (i % j == 0) continue nextPrime; // not a prime, go next i
+  }
+
+  console.log( i ); // a prime
+}
